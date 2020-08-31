0e484b784b8154d7d2eb63a86af08f1eeac6b4d2
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2020-08-31 13:33:17 +0200

    update

--- projet/projet/settings.py
+++ projet/projet/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 import dj_database_url
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -28,7 +27,7 @@ SECRET_KEY = '6!jds3@!t5yku9*wxk70qk7cidj2jk^7phs5mqh!&9v1gmnrs6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['joebloodbank.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bloodbankjoel.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -92,23 +91,23 @@ WSGI_APPLICATION = 'projet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bloodbankdb',
-        'USER': 'joelle',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        
-    }
-}
-
 #DATABASES = {
     #'default': {
-       #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+       # 'ENGINE': 'django.db.backends.postgresql',
+       # 'NAME': 'bloodbankdb',
+       # 'USER': 'joelle',
+        #'PASSWORD': 'password',
+       # 'HOST': 'localhost',
+        
+   # }
 #}
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -167,3 +166,5 @@ AUTHENTICATION_BACKENDS = [
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals(), logging=False)
